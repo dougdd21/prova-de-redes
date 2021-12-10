6e9222852cd6e6f27338dee716bc9550dc2ac45f
dougdd21 <40614378+dougdd21@users.noreply.github.com>
2021-12-10 20:45:16 -0300

    cliente

---
+++ cliente.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+
+if len(sys.argv) == 3:
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+else:
+    print("Run like : python3 client.py <arg1 server ip 192.168.1.102> <arg2 server port 4444 >")
+    exit(1)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+print("Do Ctrl+c to exit the program !!")
+
+while True:
+    send_data = input("Type some text to send =>");
+    s.sendto(send_data.encode('utf-8'), (ip, port))
+    print("\n\n 1. Client Sent : ", send_data, "\n\n")
+    data, address = s.recvfrom(4096)
+    print("\n\n 2. Client received : ", data.decode('utf-8'), "\n\n")
+s.close()
