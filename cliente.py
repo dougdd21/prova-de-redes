7fb809cca2a6c9d62c410fce9547cbd177602fa0
dougdd21 <40614378+dougdd21@users.noreply.github.com>
2021-12-10 20:39:16 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -2,25 +2,20 @@ import socket
 import sys
 
 if len(sys.argv) == 3:
-    # Get "IP address of Server" and also the "port number" from
-    argument 1 and argument 2
     ip = sys.argv[1]
     port = int(sys.argv[2])
 else:
-    print("Run like : python3 server.py <arg1:server ip:this system IP 192.168.1.6> <arg2:server port:4444 >")
+    print("Run like : python3 client.py <arg1 server ip 192.168.1.102> <arg2 server port 4444 >")
     exit(1)
 
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-server_address = (ip, port)
-s.bind(server_address)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
 print("Do Ctrl+c to exit the program !!")
 
 while True:
-    print("####### Server is listening #######")
+    send_data = input("Type some text to send =>");
+    s.sendto(send_data.encode('utf-8'), (ip, port))
+    print("\n\n 1. Client Sent : ", send_data, "\n\n")
     data, address = s.recvfrom(4096)
-    print("\n\n 2. Server received: ", data.decode('utf-8'), "\n\n")
-    send_data = input("Type some text to send => ")
-    s.sendto(send_data.encode('utf-8'), address)
-    print("\n\n 1. Server sent : ", send_data,"\n\n")
+    print("\n\n 2. Client received : ", data.decode('utf-8'), "\n\n")
+    
+s.close()
